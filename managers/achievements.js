404d5c35feb3ab664d651562e1cf8aaa659e4b01
Thanasis Papoutsidakis <blastofftek@gmail.com>
2024-08-27 12:40:12 +0300

    Add achievement popups to the game

--- managers/achievements.js
+++ managers/achievements.js
@@ -1,3 +1,5 @@
+import {achievementsData} from "../data/achievements.js";
+
 const ACHIEVEMENTS_STORAGE_KEY = 'systemsim-achievements';
 
 export class Achievements {
@@ -5,11 +7,13 @@ export class Achievements {
     const achievements = localStorage.getItem(ACHIEVEMENTS_STORAGE_KEY);
     if (!achievements) {
       localStorage.setItem(ACHIEVEMENTS_STORAGE_KEY, JSON.stringify([name]));
+      Achievements.showAchievementPopup(name);
     } else {
       const achievementsList = JSON.parse(achievements);
       if (!achievementsList.includes(name)) {
         achievementsList.push(name);
         localStorage.setItem(ACHIEVEMENTS_STORAGE_KEY, JSON.stringify(achievementsList));
+        Achievements.showAchievementPopup(name);
       }
     }
   }
@@ -19,5 +23,26 @@ export class Achievements {
     if (!achievements) return false;
     const achievementsList = JSON.parse(achievements);
     return achievementsList.includes(name);
+  }
+
+  static showAchievementPopup(name) {
+    const achievement = achievementsData.get(name);
+    if (!achievement) return;
+
+    document.getElementById('new-achievement-title').innerHTML = achievement.title;
+    document.getElementById('new-achievement-description').innerHTML = achievement.description;
+    document.getElementById('achievements-popup').style.display = null;
+
+    setTimeout(() => {
+      document.getElementById('achievements-popup').style.opacity = 1;
+    });
+
+    setTimeout(() => {
+      document.getElementById('achievements-popup').style.opacity = 0;
+    }, 4000);
+
+    setTimeout(() => {
+      document.getElementById('achievements-popup').style.display = 'none';
+    }, 5000);
   }
 }
